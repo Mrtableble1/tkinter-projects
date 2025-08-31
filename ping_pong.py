2eac82ccc1b01b032850accd5c63ea7350eb0798
Mrtableble1 <knit.aryan@gmail.com>
2025-08-31 10:27:26 +0200

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -15,17 +15,20 @@ score = canvas.create_text(400,50,font=("Arial",50,"bold"),fill="white",text="0:
 
 
 class Ball():
-    def __init__(self):
+    def __init__(self,padel1,padel2):
         self.ball= canvas.create_oval(0,0,50,50,fill="blue")
         self.bally = +4
         self.ballx = +4
         self.score1 = 0
         self.score2 = 0
-        
+        self.padel1 = padel1
+        self.padel2 = padel2
     
     def draw(self):
         canvas.move(self.ball,self.ballx,self.bally)
         self.pos = canvas.coords(self.ball)
+        self.pos1 = canvas.coords(self.padel1)
+        self.pos2 = canvas.coords(self.padel2)
 
         if self.pos[0] <= 0:
             self.ballx =+ 4
@@ -40,11 +43,23 @@ class Ball():
 
         if self.pos[3] >= 600:
             self.bally =- 4
-        
+
         canvas.itemconfigure(score,text=str(self.score1)+":"+str(self.score2))
+        
+
+    def bounce(self):
+        self.pos = canvas.coords(self.ball)
+        self.pos1 = canvas.coords(self.padel1.padel)
+        self.pos2 = canvas.coords(self.padel2.padel)
+        if self.pos1[1] <= self.pos[3] and self.pos1[3] >= self.pos[1]:
+            if self.pos1[0]<= self.pos[0] and self.pos1[2] >=self.pos[0]:
+                self.ballx = 4
+
+        if self.pos2[1] <= self.pos[3] and self.pos2[3] >= self.pos[1]:
+            if self.pos2[0]<= self.pos[2] and self.pos2[0] >=self.pos[2]:
+                self.ballx = -4
 
 
-ball1 = Ball()
 
 class Padel():
     def __init__(self,x0,y0,x1,y1):
@@ -60,11 +75,11 @@ class Padel():
             self.changeY = 0
         
     def up(self,event):
-        changeY =-4
+        self.changeY =-4
 
 
     def down(self,event):
-        changeY =+4
+        self.changeY =+4
 
     def move_up_down(self,keyup,keydown):
         canvas.bind_all(keyup,self.up)
@@ -79,7 +94,10 @@ class Padel():
 padel1 = Padel(10,250,30,350)
 padel2 = Padel(770,250,790,350)
 
+ball1 = Ball(padel1,padel2)
+
 padel1.move_up_down('w','s')
+padel2.move_up_down('<KeyPress-Up>','<KeyPress-Down>')
 
 x = 0
 
@@ -87,8 +105,17 @@ while x == 0:
     ball1.draw()
     padel1.draw()
     padel2.draw()
+    ball1.bounce()
+    if ball1.score1 == 11:
+        canvas.create_text(400,100,font=("Arial",50,"bold"),fill="white",text="player1 wins")
+        break   
+
+    if ball1.score2 == 11:
+        canvas.create_text(400,100,font=("Arial",50,"bold"),fill="white",text="player2 wins")
+        break
     screen.update_idletasks()
     screen.update()
+
 
 
 screen.mainloop()
